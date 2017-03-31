876c7db10da45b1caa3ec274ef4f26aafa0d6d83
lucas henrique <lucashsc31@gmail.com>
2017-03-31 19:41:11 -0300

    estrutura html feita

--- desafio10/desafio10.php
+++ desafio10/desafio10.php
@@ -12,16 +12,31 @@ Faça a validação dos campos de modo que usuário tenha que preencher todos os
 <head>
     <meta charset="UTF-8">
     <title>Desafio 10</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 </head>
 <body>
 
   <div class="container">
 
-      <div class="col-sm-6">
+      <div class="col-md-6">
 
         <h1 class="text-center">DESAFIO 10 - NOTAS</h1>
 
+        <form method="get" action="desafio10.php">
+          <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
+          </div>
+          <div class="form-group">
+            <label for="nota1">Nota 1</label>
+            <input type="text" class="form-control" id="nota1" name="nota1" placeholder="Nota 1">
+          </div>
+          <div class="form-group">
+            <label for="nota2">Nota 2</label>
+            <input type="text" class="form-control" id="nota2" name="nota2" placeholder="Nota 2">
+          </div>
+          <button type="submit" class="btn btn-primary">Calcular Média</button>
+        </form>
 
     </div>
   </div>
