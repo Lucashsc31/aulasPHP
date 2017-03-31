607116cb3ba7bb37d233986f26e3b1344231432c
lucas henrique <lucashsc31@gmail.com>
2017-03-31 20:10:31 -0300

    exercicio cookie

---
+++ aula-cookies/cookies.php
@@ -0,0 +1,22 @@
+<?php
+
+//SETANDO COOKIE
+
+$nome = "Nome do Cookie";
+$valor = 200;
+$expira = time() + (60 * 60 * 24 * 7);
+
+setcookie($nome, $valor, $expira);
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>COOKIES</title>
+</head>
+<body>
+
+
+</body>
+</html>
